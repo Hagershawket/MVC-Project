8fbbfb53abac123ff50a3af1f473041fe094a89e
Hagershawket <hagershawkat@gmail.com>
2024-09-20 21:01:41 +0300

    Part 06 Department Controller - Details

--- LinkDev.IKEA.PL/Controllers/DepartmentController.cs
+++ LinkDev.IKEA.PL/Controllers/DepartmentController.cs
@@ -70,5 +70,18 @@ namespace LinkDev.IKEA.PL.Controllers
                 }
             }
         }
+
+        [HttpGet] // /Department/Details
+        public IActionResult Details(int? id)
+        {
+            if (id is null)
+                return BadRequest();
+
+            var department = _departmentService.GetDepartmentById(id.Value);
+            if (department is null)
+                return NotFound();
+
+            return View(department);
+        }
     }
 }
