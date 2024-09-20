0ec588a3073d6af81f8f4e97442071a153e89baa
Hagershawket <hagershawkat@gmail.com>
2024-09-20 19:52:52 +0300

    Part 05 Department Controller - Create

--- LinkDev.IKEA.BLL/Models/Departments/CreatedDepartmentDto.cs
+++ LinkDev.IKEA.BLL/Models/Departments/CreatedDepartmentDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,12 @@ namespace LinkDev.IKEA.BLL.Models.Departments
 {
     public class CreatedDepartmentDto
     {
+        [Required(ErrorMessage = "Code is Required ya Hamda!!")]
         public string Code { get; set; } = null!;
         public string Name { get; set; } = null!;
         public string? Description { get; set; }
+
+        [Display(Name = "Date Of Creation")]
         public DateOnly CreationDate { get; set; }
     }
 }
--- LinkDev.IKEA.PL/Controllers/DepartmentController.cs
+++ LinkDev.IKEA.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
-﻿using LinkDev.IKEA.BLL.Services.Departments;
+﻿using LinkDev.IKEA.BLL.Models.Departments;
+using LinkDev.IKEA.BLL.Services.Departments;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace LinkDev.IKEA.PL.Controllers
 {
@@ -8,15 +10,65 @@ namespace LinkDev.IKEA.PL.Controllers
     public class DepartmentController : Controller
     {
         private readonly IDepartmentService _departmentService;
-        public DepartmentController(IDepartmentService departmentService)
+        private readonly ILogger<DepartmentController> _logger;
+        private readonly IWebHostEnvironment _environment;
+        public DepartmentController(IDepartmentService departmentService, ILogger<DepartmentController> logger, IWebHostEnvironment environment)
         {
             _departmentService = departmentService;
+            _logger = logger;
+            _environment = environment;
         }
+
         [HttpGet] // GET: /Department/Index
         public IActionResult Index()
         {
             var departments = _departmentService.GetAllDepartments();
             return View(departments);
+        }
+
+        [HttpGet] // GET: /Department/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] // POST
+        public IActionResult Create(CreatedDepartmentDto department)
+        {
+            if (!ModelState.IsValid)            // Server-Side Validation
+                return View(department);
+
+            string message = string.Empty;
+            try
+            {
+                var result = _departmentService.CreateDepartment(department);
+                if (result > 0)
+                    return RedirectToAction(nameof(Index));
+                else
+                {
+                    message = "Department is not Created";
+                    ModelState.AddModelError(string.Empty, message);
+                    return View(department);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // 1. Log Exception
+                _logger.LogError(ex, ex.Message);
+
+                // 2. Set Message
+                if (_environment.IsDevelopment())
+                {
+                    message = ex.Message;
+                    return View(department);
+                }
+                else
+                {
+                    message = "Department is not Created";
+                    return View("Error", message);
+                }
+            }
         }
     }
 }
